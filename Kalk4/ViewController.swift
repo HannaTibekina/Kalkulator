53f93d85a294173d13dc2caefa5c646874378e19
gilbert <annatibekina3@gmail.com>
2022-06-06 12:34:01 +0300

    Initial Commit

---
+++ Kalk4/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Kalk4
+//
+//  Created by Анна Тибекина on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberForScreem: Double = 0;
+    var firstNum: Double = 0
+    var operation: Int = 0
+    var mathSign: Bool = false
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        if mathSign == true {
+            result.text = String(sender.tag)
+            mathSign = false
+        }
+        else {
+            result.text = result.text! + String(sender.tag)
+        }
+        
+        numberForScreem = Double(result.text!)!
+    }
+    
+    @IBAction func Buttons(_ sender: UIButton) {
+        if result.text != "" && sender.tag != 10 && sender.tag != 15 {
+            firstNum = Double(result.text!)!
+            
+            
+            if sender.tag == 11 { // Деление
+                result.text = "/"
+            }
+            else if sender.tag == 12 { // Умножение
+                result.text = "x"
+        }
+            else if sender.tag == 13 { //Вычитание
+                result.text = "-"
+                }
+            else if sender.tag == 14 { //Добавление
+                result.text = "+"
+            }
+            
+        operation = sender.tag
+        mathSign = true
+    }
+    else if sender.tag == 15 { //Посчитать
+        if operation == 11 {
+            result.text = String(firstNum / numberForScreem)
+        }
+        else if operation == 12 {
+            result.text = String(firstNum * numberForScreem)
+        }
+        else  if operation == 13 {
+            result.text = String(firstNum - numberForScreem)
+        }
+        else  if operation == 14 {
+            result.text = String(firstNum + numberForScreem)
+        }
+        }
+        else if sender.tag == 10 {
+            result.text = ""
+            operation = 0
+            firstNum = 0
+            numberForScreem = 0
+        }
+}
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
